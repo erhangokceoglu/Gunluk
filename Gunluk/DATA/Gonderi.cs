f3e126db9838e4a2739665f6fec19f8dd8f49ea8
John Doe <erhangokceoglu91@gmail.com>
2023-01-04 15:24:17 +0300

    Data

---
+++ Gunluk/DATA/Gonderi.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Gunluk.DATA
+{
+    public class Gonderi
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(400)]
+        public string Baslik { get; set; } = null!;
+
+        public string? Icerik { get; set; } = null!;
+
+        [MaxLength(255)]
+        public string? Resim { get; set; }
+
+        public DateTime OlusturulmaZamani { get; set; } = DateTime.Now;
+
+        public DateTime DegistirilmeZamani { get; set; } = DateTime.Now;
+
+        public int KategoriId { get; set; }
+
+        public Kategori Kategori { get; set; } = null!;
+    }
+}
