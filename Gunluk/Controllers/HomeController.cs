48e33cedc86fbd3ac338b083ef4430271ea24b64
John Doe <erhangokceoglu91@gmail.com>
2023-01-05 15:41:37 +0300

    pagination

--- Gunluk/Controllers/HomeController.cs
+++ Gunluk/Controllers/HomeController.cs
@@ -15,18 +15,29 @@ namespace Gunluk.Controllers
             _db = db;
         }
 
-        public IActionResult Index(int? KategoriId)
+        public IActionResult Index(int? kategoriId, int sayfa = 1)
         {
             IQueryable<Gonderi> gonderiler = _db.Gonderiler;
 
-            if (KategoriId != null)
+            if (kategoriId != null)
             {
-                gonderiler = gonderiler.Where(x => x.KategoriId == KategoriId);
-                ViewBag.Baslik = _db.Kategoriler.Find(KategoriId)?.Ad;
-
+                gonderiler = gonderiler.Where(x => x.KategoriId == kategoriId);
+                ViewBag.Baslik = _db.Kategoriler.Find(kategoriId)?.Ad;
             }
 
-            return View(gonderiler.ToList());
+            int sayfaAdet = (int)Math.Ceiling((double)gonderiler.Count() / Sabitler.SAYFA_BASINA_GONDERI);
+
+            gonderiler = gonderiler.Skip((sayfa - 1) * Sabitler.SAYFA_BASINA_GONDERI).Take(Sabitler.SAYFA_BASINA_GONDERI);
+
+            var vm = new HomeViewModel()
+            {
+                KategoriId = kategoriId,
+                Gonderiler = gonderiler.ToList(),
+                Sayfa = sayfa,
+                SayfaAdet = sayfaAdet
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
